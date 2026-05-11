1220537492d6974af1a45051ae321cf8f0245ff1
chimdi247 <Chimdi247@gmail.com>
2026-05-11 03:54:17 +0300

    Change S3 bucket name for Terraform backend

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-chimdi"  # Replace with your S3 bucket name
+    bucket       = "tf-chimdi-bucket"  # Replace with your S3 bucket name
     key          = "envs/dev/terraform.tfstate"
     region       = "eu-west-2"
     encrypt      = true
