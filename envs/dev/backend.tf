dd27eed99f40db8c9ee5c1b2460b59eadec86c91
chimdi247 <Chimdi247@gmail.com>
2026-07-06 14:44:53 +0300

    Update S3 bucket name in backend configuration

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tf-chimdi-bucket"  # Replace with your S3 bucket name
+    bucket       = "tf-chimdi247-bucket"  # Replace with your S3 bucket name
     key          = "envs/dev/terraform.tfstate"
     region       = "eu-west-2"
     encrypt      = true
